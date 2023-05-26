ca675e23ab95990e4cceac095c597394beb171b4
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-05-26 16:22:39 -0300

    foi importado algumas atualizações para implementação da tela de chat

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { io } from 'socket.io-client';
-import MessageInput from './MessageInput';
 import usersData from './data';
-import { FaCircle, FaEllipsisH, FaSearch } from 'react-icons/fa';
-
+import { FaSearch, FaEllipsisH, FaEllipsisV } from 'react-icons/fa';
+import { InputGroup, FormControl } from 'react-bootstrap';
 
 const socket = io('http://localhost:9000');
 
@@ -22,6 +21,7 @@ const ChatScreen = () => {
   const handleMouseLeave = () => {
     setHoveredUser(null);
   };
+
   useEffect(() => {
     socket.on('connect', () => {
       console.log('Conectado ao servidor de chat');
@@ -73,257 +73,285 @@ const ChatScreen = () => {
   };
 
   return (
-    <>
-      <div style={styles.header}>
-        <h2 style={styles.title}>Tela de Chat</h2>
-      </div>
-      <div style={styles.container}>
+    <div style={styles.container}>
+      <div style={styles.headerContainer}>
         <div style={styles.header}>
-          {/* Elementos do cabeçalho */}
-          <div style={styles.headerLeft}>
-            <img style={styles.whatsappLogo} src="caminho/para/seu/logo.png" alt="Logo do WhatsApp" />
-            <div style={styles.statusIndicatorContainer}>
-              <FaCircle style={styles.statusIndicator} />
-            </div>
-          </div>
+          <img
+            style={styles.whatsappLogo}
+            src="https://anagiovanna.com.br/blog/wp-content/uploads/2022/01/foto-para-perfil-do-whatsapp-4.jpg"
+            alt="Márcia Helena"
+          />
+        </div>
+      </div>
+      <div style={styles.content}>
+
+        <div style={styles.userListContainer}>
           <div style={styles.headerRight}>
+            <InputGroup>
+              <InputGroup.Text>
+                <FaSearch />
+              </InputGroup.Text>
+              <FormControl placeholder="Search" style={{ marginLeft: '10px' }} />
+            </InputGroup>
             <FaEllipsisH style={styles.menuIcon} />
           </div>
-        </div>
-        <div style={styles.searchBar}>
-          <FaSearch style={styles.searchIcon} />
-          <input
-            type="text"
-            placeholder="Pesquisar ou começar novas conversas"
-            style={styles.searchInput}
-          />
-          <div style={styles.userListContainer}>
-            <h3 style={styles.userListTitle}>Usuários Online:</h3>
+          <div style={styles.userListTitle}>Conversas</div>
+          <div>
             <ul style={styles.userList}>
               {users.map((user) => (
                 <li
                   key={user.id}
-                  className={`${styles.userListItem} ${hoveredUser === user.id ? styles.userListItemHover : ""}`}
+                  className={`${styles.userListItem} ${hoveredUser === user.id ? styles.userListItemHover : ""
+                    }`}
                   onMouseEnter={() => handleMouseEnter(user.id)}
                   onMouseLeave={() => handleMouseLeave(user.id)}
                 >
                   <div style={styles.userAvatarContainer}>
                     <img style={styles.userAvatar} src={user.photoUrl} alt={user.username} />
                     {user.hasNewMessages && <div style={styles.newMessagesIndicator} />}
+
+                    <div style={styles.userInfo}>
+                      <h4 style={styles.username}>{user.username}</h4>
+                      <p style={styles.lastMessage}>{user.lastMessage}</p>
+                    </div>
                   </div>
-                  <div style={styles.userInfo}>
-                    <h4 style={styles.username}>{user.username}</h4>
-                    <p style={styles.status}>{user.status}</p>
-                  </div>
+
                 </li>
               ))}
             </ul>
-          </div>
-          <div style={styles.messageContainer}>
-            <h3 style={styles.messageListTitle}>Mensagens:</h3>
-            <ul style={styles.messageList}>
-              {messages.map((message, index) => (
-                <li
-                  key={index}
-                  style={
-                    message.isOwn
-                      ? styles.messageListItemOwn
-                      : styles.messageListItem
-                  }
-                >
-                  {message.content}
-                </li>
-              ))}
-            </ul>
-            <div style={styles.buttonContainer}>
-              <MessageInput onSendMessage={handleSendMessage} />
-              <form onSubmit={handleSendMessage}>
-                <input
-                  type="text"
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  placeholder="Digite sua mensagem"
-                  style={styles.input}
-                />
-                <button type="submit" style={styles.button}>
-                  Enviar
-                </button>
-              </form>
-              <button style={styles.logoutButton} onClick={handleLogout}>
-                Sair
-              </button>
-            </div>
           </div>
         </div>
+        <div style={styles.chatContainer}>
+          <div style={styles.chatHeader}>
+            <h2 style={styles.chatTitle}>Nome do Contato</h2>
+            <FaEllipsisV style={styles.chatMenuIcon} />
+          </div>
+          <div style={styles.messageList}>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                style={message.isOwn ? styles.messageItemOwn : styles.messageItem}
+              >
+                <p style={styles.messageContent}>{message.content}</p>
+              </div>
+            ))}
+          </div>
+          <form onSubmit={handleSendMessage} style={styles.messageInputContainer}>
+            <input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Digite sua mensagem"
+              style={styles.input}
+            />
+            <button type="submit" style={styles.button}>
+              Enviar
+            </button>
+            <button style={styles.logoutButton} onClick={handleLogout}>
+              Sair
+            </button>
+          </form>
+
+        </div>
       </div>
-    </>
+    </div>
   );
- }  
+}
 
 const styles = {
   container: {
-    display: "flex",
-    alignItems: "stretch",
-    marginTop: "20px",
-    background: "#f8f9fa", // Cor de fundo clara
-    height: "100vh", // Altura total da tela
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    backgroundColor: '#f8f9fa',
+  },
+  headerContainer: {
+    flex: '0 0 auto',
+    width: '320px', // Defina a largura desejada aqui
   },
   header: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between", // Alinhar espaço entre os elementos
-    width: "100%",
-    padding: "10px",
-    backgroundColor: "#075e54", // Cor do cabeçalho do WhatsApp
-    color: "white",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '10px',
+    backgroundColor: '#075e54',
+    color: 'white',
   },
-  title: {
-    fontSize: "20px",
-    margin: "0", // Adicionado margem zero
+  lastMessage: {
+    margin: '0',
+    fontSize: '14px',
+    color: '#999999',
   },
-  buttonContainer: {
-    display: "flex", // Adicionado display flex
-    alignItems: "center", // Alinhar verticalmente
+  whatsappLogo: {
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    marginRight: '10px',
   },
-  logoutButton: {
-    padding: "5px 10px",
-    backgroundColor: "#128c7e", // Cor do botão de sair
-    color: "white",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-    marginLeft: "10px", // Espaçamento à esquerda
+  headerRight: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '16px',
+  },
+  searchIcon: {
+    marginRight: '10px',
+    fontSize: '20px',
+  },
+  menuIcon: {
+    fontSize: '20px',
+  },
+  content: {
+    display: 'flex',
+    flex: '1',
   },
   userListContainer: {
-    flex: "0 0 300px",
-    padding: "10px",
-    backgroundColor: "#f8f9fa",
-    borderRight: "1px solid #ddd",
+    flex: '0 0 300px',
+    padding: '10px',
+    backgroundColor: '#f8f9fa',
+    borderRight: '1px solid #ddd',
+    overflowY: 'auto', // Adicionado para permitir rolagem vertical
   },
   userListTitle: {
-    margin: "0",
-    padding: "10px",
-    fontSize: "18px",
-    fontWeight: "bold",
-    color: "#4a4a4a",
+    margin: '0',
+    padding: '10px',
+    fontSize: '18px',
+    fontWeight: 'bold',
+    color: '#4a4a4a',
+    borderBottom: '1px solid #ddd', // Adicionado para separar o título da lista
   },
-
   userList: {
-    margin: "0",
-    padding: "0",
-    listStyleType: "none",
+    margin: '0',
+    padding: '0',
+    listStyleType: 'none',
   },
-  userListItemActive: {
-    marginBottom: "5px",
-    padding: "5px 10px",
-    borderRadius: "5px",
-    backgroundColor: "#075e54", // Cor de fundo dos itens da lista de usuários ativos
-    color: "#fff", // Cor do texto dos itens da lista de usuários ativos
-    cursor: "pointer",
-  },
-  messageContainer: {
-    flex: "1",
-    display: "flex",
-    flexDirection: "column",
-    backgroundColor: "#fff", // Cor de fundo da lista de mensagens
-  },
-  messageListTitle: {
-    margin: "0",
-    marginBottom: "10px",
-    fontSize: "16px",
-    fontWeight: "bold",
-    color: "#075e54", // Cor do título da lista de mensagens
-    padding: "10px",
-    backgroundColor: "#075e54", // Cor de fundo do cabeçalho da lista de mensagens
-  },
-  messageList: {
-    flex: "1",
-    padding: "10px",
-    overflowY: "auto",
-  },
-  messageListItem: {
-    marginBottom: "5px",
-    padding: "10px",
-    borderRadius: "10px",
-    backgroundColor: "#e2ffc7", // Cor de fundo dos itens da lista de mensagens
-  },
-  messageListItemOwn: {
-    marginBottom: "5px",
-    padding: "10px",
-    borderRadius: "10px",
-    backgroundColor: "#dcf8c6", // Cor de fundo das próprias mensagens
-    alignSelf: "flex-end", // Alinhar à direita
-  },
-  messageForm: {
-    display: "flex",
-    backgroundColor: "#f8f9fa", // Cor de fundo do formulário
-    padding: "10px",
-    borderTop: "1px solid #e0e0e0", // Linha superior do formulário
-  },
-  input: {
-    flex: "1",
-    marginRight: "10px",
-    padding: "5px",
-    border: "1px solid #e0e0e0", // Borda do campo de entrada
-    borderRadius: "5px",
-    outline: "none", // Remove a borda de foco
-  },
-  button: {
-    padding: "5px 10px",
-    backgroundColor: "#128c7e", // Cor do botão de enviar
-    color: "white",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-  },
-  userAvatarContainer: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: "10px", // Espaçamento à direita
-    position: "relative", // Para posicionar o indicador de novas mensagens
-  },
-  userAvatar: {
-    width: "50px", // Largura da imagem do usuário
-    height: "50px", // Altura da imagem do usuário
-    borderRadius: "50%", // Forma arredondada para a foto do usuário
-    marginRight: "10px",
-  },
-  newMessagesIndicator: {
-    position: "absolute",
-    top: "-5px", // Posicionamento em relação ao topo
-    right: "-5px", // Posicionamento em relação à direita
-    width: "10px", // Largura do indicador
-    height: "10px", // Altura do indicador
-    borderRadius: "50%", // Forma arredondada para o indicador
-    backgroundColor: "#128c7e", // Cor do indicador de novas mensagens
-  },
-  userInfo: {
-    flex: "1", // Ocupar espaço restante horizontalmente
-  },
-  username: {
-    margin: "0",
-    fontSize: "16px", // Tamanho da fonte do nome de usuário
-    fontWeight: "bold", // Negrito para o nome de usuário
-    color: "#075e54", // Cor do nome de usuário
-  },
-  status: {
-    margin: "0",
-    fontSize: "14px",
-    color: "#999999",
-  },
-
   userListItem: {
-    display: "flex",
-    alignItems: "center",
-    padding: "10px",
-    backgroundColor: "#fff",
-    cursor: "pointer",
-    transition: "background-color 0.3s",
+    display: 'flex',
+    alignItems: 'center',
+    padding: '10px',
+    backgroundColor: '#fff',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+    borderBottom: '1px solid #ddd', // Adicionado para separar os itens da lista
   },
   userListItemHover: {
-    backgroundColor: "#f2f2f2",
+    backgroundColor: '#f2f2f2',
   },
-  
+  userAvatarContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '10px',
+    position: 'relative', // Adicionado para posicionar o indicador de novas mensagens
+    borderBottom: '1px solid #ddd', // Adicionado para separar o título da lista
+    padding: '10px',
+  },
+  userAvatar: {
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    marginRight: '10px',
+  },
+  ewMessagesIndicator: {
+    position: 'absolute',
+    top: '50%',
+    right: '-5px',
+    transform: 'translateY(-50%)', // Adicionado para centralizar verticalmente
+    width: '10px',
+    height: '10px',
+    borderRadius: '50%',
+    backgroundColor: '#128c7e',
+  },
+  userInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '1', // Adicionado para ocupar o espaço disponível
+  },
+  username: {
+    margin: '0',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: '#075e54',
+  },
+  status: {
+    margin: '0',
+    fontSize: '14px',
+    color: '#999999',
+  },
+  chatContainer: {
+    flex: '1',
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+  },
+  chatHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: '10px',
+    backgroundColor: '#075e54',
+    color: 'white',
+  },
+  chatTitle: {
+    margin: '0',
+    fontSize: '16px',
+  },
+  chatMenuIcon: {
+    fontSize: '20px',
+  },
+  messageList: {
+    flex: '1',
+    padding: '10px',
+    overflowY: 'auto',
+  },
+  messageItem: {
+    marginBottom: '5px',
+    padding: '10px',
+    borderRadius: '10px',
+    backgroundColor: '#e2ffc7',
+  },
+  messageItemOwn: {
+    marginBottom: '5px',
+    padding: '10px',
+    borderRadius: '10px',
+    backgroundColor: '#dcf8c6',
+    alignSelf: 'flex-end',
+  },
+  messageContent: {
+    margin: '0',
+  },
+  messageInputContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '10px',
+    borderTop: '1px solid #e0e0e0',
+  },
+  input: {
+    flex: '1',
+    marginRight: '10px',
+    padding: '5px',
+    border: '1px solid #e0e0e0',
+    borderRadius: '5px',
+    outline: 'none',
+  },
+  button: {
+    padding: '5px 10px',
+    backgroundColor: '#128c7e',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  },
+  logoutButton: {
+    padding: '5px 10px',
+    backgroundColor: '#128c7e',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    marginLeft: '10px',
+  },
 };
+
 
 export default ChatScreen;
--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,22 +1,28 @@
 const users = [
-    {
-      id: 1,
-      username: "Daniel Irmão",
-      photoUrl: "https://anagiovanna.com.br/blog/wp-content/uploads/2022/01/foto-para-perfil-do-whatsapp-18.jpg",
-      status: "Disponível",
-    },
-    {
-      id: 2,
-      username: "Joana Cunhada",
-      photoUrl: "https://conteudo.imguol.com.br/c/entretenimento/46/2022/11/16/selfie-e-tambem-uma-maneira-de-autoconhecimento-diz-a-fotograda-maria-emilia-dinat-1668626272172_v2_1x1.jpg",
-      status: "Ausente",
-    },
-    {
-      id: 3,
-      username: "Elmer",
-      photoUrl: "https://i.pinimg.com/564x/89/6b/b1/896bb1de7605403048eeecde381ac343.jpg",
-      status: "Indisponível",
-    },
+  {
+    id: 1,
+    username: "Daniel Irmão",
+    photoUrl: "https://anagiovanna.com.br/blog/wp-content/uploads/2022/01/foto-para-perfil-do-whatsapp-18.jpg",
+    status: "Disponível",
+    lastMessage: "Olá, como você está?",
+    hasNewMessages: true,
+  },
+  {
+    id: 2,
+    username: "Joana Cunhada",
+    photoUrl: "https://conteudo.imguol.com.br/c/entretenimento/46/2022/11/16/selfie-e-tambem-uma-maneira-de-autoconhecimento-diz-a-fotograda-maria-emilia-dinat-1668626272172_v2_1x1.jpg",
+    status: "Ausente",
+    lastMessage: "Tudo bem, e com você?",
+    hasNewMessages: false,
+  },
+  {
+    id: 3,
+    username: "Elmer",
+    photoUrl: "https://i.pinimg.com/564x/89/6b/b1/896bb1de7605403048eeecde381ac343.jpg",
+    status: "Indisponível",
+    lastMessage: "Não se preocupe, eu entendo.",
+    hasNewMessages: true,
+  },
     // Adicione mais objetos de usuário aqui conforme necessário
   ];
   
